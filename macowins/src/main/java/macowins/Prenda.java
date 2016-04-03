cc3948fc7c89bbb18b014802aa637c5b43042afe
mbondarenco <maru.bondarenco@gmail.com>
2016-04-03 17:41:59 -0300

    El metodo precioFinal ahora esta en Prenda

--- macowins/src/main/java/macowins/Prenda.java
+++ macowins/src/main/java/macowins/Prenda.java
@@ -1,18 +1,19 @@
 package macowins;
 
 public abstract class Prenda {
-	
 
-		public double valorFijoNegocio() {
-			return 5.0;
+	protected double valorFijoNegocio = 5.0;
+	protected double aplicaTasaImportacion = 1.30;
+	protected int precioBase = 0;
+	protected boolean esImportado = true;
+
+	public double precioFinal() {
+		double valorRealFinal = 0;
+		if (esImportado) {
+			valorRealFinal = ((valorFijoNegocio + precioBase) * aplicaTasaImportacion);
+		} else {
+			valorRealFinal = (valorFijoNegocio + precioBase);
 		}
-
-		public double aplicaTasaImportacion = 1.30;
-		public int precioBase;
-		public boolean esImportado;
-
+		return valorRealFinal;
 	}
-
-	
-	
-
+}

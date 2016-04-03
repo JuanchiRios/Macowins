945acd6bbab548515dc5702009d73774f082ea42
mbondarenco <maru.bondarenco@gmail.com>
2016-04-03 17:48:22 -0300

    Cambio el metodo crearVenta

--- macowins/src/main/java/macowins/Macowins.java
+++ macowins/src/main/java/macowins/Macowins.java
@@ -6,50 +6,18 @@ import java.util.List;
 public class Macowins {
 
 	List<Venta> ventas = new ArrayList<Venta>();
-	public Venta inicializarVenta(int cantidad) {
-		Venta venta = new Venta();
-		ventas.add(venta);
-		venta.cantidadPrendas = cantidad;
-		return venta;
-	}
-	
-	
-	public double crearVenta(int cantidad, String tipoPrenda) {
 
-		if (tipoPrenda == "Saco") {
-			Venta venta = inicializarVenta(cantidad);
-			Saco saco = new Saco();
-			venta.agregarPrenda(saco);
-			venta.precioTotal = venta.cantidadPrendas * saco.precioFinal();
-			return venta.precioTotal;
+	public void crearVenta(Prenda prenda, int cantidad, int fecha) {
 
-		} else if (tipoPrenda == "Camisa") {
-			Venta venta = inicializarVenta(cantidad);
-			Camisa camisa = new Camisa();
-			venta.agregarPrenda(camisa);
-			venta.precioTotal = venta.cantidadPrendas * camisa.precioFinal();
-			return venta.precioTotal;
-
-		} else if (tipoPrenda == "Pantalon") {
-
-			Venta venta = inicializarVenta(cantidad);
-			Pantalon pantalon = new Pantalon();
-			venta.agregarPrenda(pantalon);
-			venta.precioTotal = venta.cantidadPrendas * pantalon.precioFinal();
-			return venta.precioTotal;
-
-		} else {
-			return -1;
-		}
+		Venta venta = new Venta(prenda, cantidad, fecha);
+		this.ventas.add(venta);
 
 	}
 
-	
-
-	public double calcularGanancias(){
+	public double calcularGanancias() {
 		double ganancias = 0;
-		for (int i = 0; i < ventas.size(); i++){
-			ganancias = ganancias + ventas.get(i).precioTotal;
+		for (int i = 0; i < ventas.size(); i++) {
+			ganancias = ganancias + ventas.get(i).getPrecioTotal();
 		}
 		return ganancias;
 	}
--- macowins/src/main/java/macowins/Venta.java
+++ macowins/src/main/java/macowins/Venta.java
@@ -5,13 +5,42 @@ import java.util.List;
 
 public class Venta {
 
-	public int cantidadPrendas;
-	public double precioTotal;
-
-	public void agregarPrenda(Prenda prenda) {
-		List<Prenda> prendas = new ArrayList<Prenda>();
-		prendas.add(prenda);
-
+	private int cantidadPrendas;
+	private double precioTotal;
+	private int fecha;
+	List<Prenda> prendas = new ArrayList<Prenda>();
+	
+	public Venta(Prenda prenda, int cantidad, int fecha){
+		this.setCantidadPrendas(cantidad);
+		this.prendas.add(prenda);
+		this.setPrecioTotal(prenda.precioFinal() * cantidad);
+		this.setFecha(fecha);
 	}
+
+	public int getCantidadPrendas() {
+		return cantidadPrendas;
+	}
+
+	public void setCantidadPrendas(int cantidadPrendas) {
+		this.cantidadPrendas = cantidadPrendas;
+	}
+
+	public double getPrecioTotal() {
+		return precioTotal;
+	}
+
+	public void setPrecioTotal(double precioTotal) {
+		this.precioTotal = precioTotal;
+	}
+
+	public int getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(int fecha) {
+		this.fecha = fecha;
+	}
+	
+
 
 }
